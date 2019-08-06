c664bdd2f380ddc3cfe8f0c10b04942666ee6980
kvokka <kvokka@yahoo.com>
2019-08-06 15:49:06 +0100

    Move the logic to lib from exe

--- lib/rubocop_on_github.rb
+++ lib/rubocop_on_github.rb
@@ -1,6 +1,27 @@
 require "rubocop_on_github/version"
+require "rubocop_on_github/options"
+
+require 'optparse'
+require 'ostruct'
 
 module RubocopOnGithub
   class Error < StandardError; end
-  # Your code goes here...
+
+
+  def run(args = ARGV)
+    @options = Options.new(args).parse!
+
+    verify_hub_version(@options.hub_version)
+  end
+
+  private
+
+  def verify_hub_version(version)
+    matches = `hub  --version`.match(/hub version (?<hub_version>(.*))/)
+    installed_version = Gem::Version.new matches.fetch('hub_version')
+    return true if installed_version >= Gem::Version.new(version)
+    warn 'Script was tested with hub version #{HUB_VERSION}'
+  rescue => e
+    raise ['Script requires https://github.com/github/hub', e.message].join("\n")
+  end
 end
---
+++ lib/rubocop_on_github/options.rb
@@ -0,0 +1,51 @@
+module RubocopOnGithub
+  class Options
+    attr_reader :options
+
+    def initialize(args)
+      @args = args
+    end
+
+    def parse # rubocop:disable Metrics/MethodLength
+      @options = OpenStruct.new
+
+      @parser = OptionParser.new do |opts|
+        opts.banner = 'Usage: create_rubocop_prs [options]'
+
+        @options.hub_version = '2.12.3'
+        opts.on('-u [version] ','--hub-version [version]', 'Set manually minimum required version of `hub` utility (default: 2.12.3)') do |v|
+          @options.hub_version = v
+        end
+
+        @options.rubocop_todo_branch = 'rubocop_todo_branch'
+        opts.on('-b [branch]', '--branch [branch]', String, 'internal branch with `.rubocop_todo.yml` (default `rubocop_todo_branch`)') do |v|
+          @options.rubocop_todo_branch = v
+        end
+
+        @options.post_checkout = ''
+        opts.on('-r [command]', '--post-checkout [command]', String, 'Running after each git checkout (default "")') do |v|
+          @options.seats = v
+        end
+
+        @options.limit = 10
+        opts.on('-t [limit]', '--limit [limit]', Integer, "Limit the PS's for one run") do |v|
+          @options.limit = v
+        end
+
+        @options.debug = false
+        opts.on('--debug', 'Output debugging information (default false)') do |_v|
+          @options.debug = true
+        end
+
+        opts.on_tail('-h', '--help', 'Display help') do
+          puts opts
+          exit
+        end
+      end
+
+      parser.parse!(@args)
+
+      @options
+    end
+  end
+end
