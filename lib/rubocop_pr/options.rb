b702c1d33892797e23fc8a7807b5c96cfc41f567
kvokka <kvokka@yahoo.com>
2019-08-12 18:18:17 +0100

    Refactored CLI options

--- lib/rubocop_pr/options.rb
+++ lib/rubocop_pr/options.rb
@@ -15,26 +15,26 @@ module RubocopPr
       options
     end
 
-    private
+    class BuildParser
+      def initialize
 
-    def build_parser # rubocop:disable Metrics/MethodLength
-      @parser = OptionParser.new do |opts|
-        opts.banner = 'Usage: rubocop_pr [options]'
-
-        add_hub_version_option(opts)
-        add_rubocop_todo_branch_option(opts)
-        add_master_branch_option(opts)
-        add_post_checkout_option(opts)
-        add_limit_option(opts)
-        add_repository_option(opts)
-        add_git_origin_option(opts)
-        add_continue_option(opts)
-        add_version_option(opts)
-        add_on_tail(opts)
       end
     end
 
-    def add_limit_option(opts)
+    private
+
+    attr_accessor :opts
+
+    def build_parser # rubocop:disable Metrics/MethodLength
+      @parser = OptionParser.new do |op|
+        self.opts = op
+        opts.banner = 'Usage: rubocop_pr [options]'
+
+        private_methods(false).map(&:to_s).select{|m| m.start_with?('add_') }.each{|m| send m }
+      end
+    end
+
+    def add_limit_option
       @options.limit = 10
       msg = "Limit the PS's for one run (default: 10)"
       opts.on('-l [limit]', '--limit [limit]', Integer, msg) do |v|
@@ -42,7 +42,7 @@ module RubocopPr
       end
     end
 
-    def add_post_checkout_option(opts)
+    def add_post_checkout_option
       @options.post_checkout = ''
       msg = 'Running after each git checkout (default: "")'
       opts.on('-r [command]', '--post-checkout [command]', String, msg) do |v|
@@ -50,7 +50,7 @@ module RubocopPr
       end
     end
 
-    def add_rubocop_todo_branch_option(opts)
+    def add_rubocop_todo_branch_option
       @options.rubocop_todo_branch = 'rubocop_todo_branch'
       msg = "internal branch with '.rubocop_todo.yml' (default: 'rubocop_todo_branch')"
       opts.on('-b [branch]', '--branch [branch]', String, msg) do |v|
@@ -58,7 +58,7 @@ module RubocopPr
       end
     end
 
-    def add_master_branch_option(opts)
+    def add_master_branch_option
       @options.master_branch = 'master'
       msg = "branch which will be the base for all PR's (default: 'master')"
       opts.on('-m [branch]', '--master [branch]', String, msg) do |v|
@@ -66,7 +66,7 @@ module RubocopPr
       end
     end
 
-    def add_git_origin_option(opts)
+    def add_git_origin_option
       @options.git_origin = 'origin'
       msg = "origin option for 'git push' (default: 'origin')"
       opts.on('-o [origin]', '--origin [origin]', String, msg) do |v|
@@ -74,7 +74,7 @@ module RubocopPr
       end
     end
 
-    def add_hub_version_option(opts)
+    def add_hub_version_option
       @options.hub_version = HUB_VERSION
       msg = "Set manually minimum required version of 'hub' utility for github (default: #{HUB_VERSION})"
       opts.on('-u [version] ', '--hub-version [version]', msg) do |v|
@@ -82,7 +82,7 @@ module RubocopPr
       end
     end
 
-    def add_repository_option(opts)
+    def add_repository_option
       @options.repository = 'github'
       msg = 'Set repository host (default: github)'
       opts.on('-g [name] ', '--repository [name]', msg) do |v|
@@ -90,21 +90,21 @@ module RubocopPr
       end
     end
 
-    def add_continue_option(opts)
+    def add_continue_option
       @options.continue = false
       opts.on('-c', '--continue', 'Continue previous session (default: false)') do |_v|
         @options.continue = true
       end
     end
 
-    def add_version_option(opts)
+    def add_version_option
       opts.on('-v', '--version', 'Display version') do
         puts RubocopPr::VERSION
         exit
       end
     end
 
-    def add_on_tail(opts)
+    def add_on_tail
       opts.on_tail('-h', '--help', 'Display help') do
         puts opts
         exit
